042d435412cb92e7d3611443173ff13c9bc35a77
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2020-09-08 20:59:52 -0300

    Subida de archivos

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+app.listen(3000)
+
+app.get("/", function(req, res) {
+        res.send(`Ni Superman, Iron
+        Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y
+        hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como
+        inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en
+        ti!`)
+})
+
+app.get("/heroes", function(req, res) {
+    let archivoHeroes = path.resolve("heroes.json")    
+    res.sendFile(archivoHeroes)
+})
+
+app.get("/creditos", function(req, res) {
+    res.send(`Developer: Oscar Ariel Saucedo ----
+    Trabajo finalizado`)
+})
