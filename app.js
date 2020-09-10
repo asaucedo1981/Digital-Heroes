b756413733a2e94f2bb298c927664c64e84da153
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2020-09-10 19:06:56 -0300

    Agregado de comentarios

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+/*  Instanciar variables a utilizar - Carga de modulo Express */
 const express = require("express")
 const app = express()
 const path = require("path")
 
+/* Ruta raiz */
 app.get("/", function(req, res) {
         res.send(`Ni Superman, Iron
         Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y
@@ -10,18 +12,22 @@ app.get("/", function(req, res) {
         ti!`)
 })
 
+/* Pagina Heroes */
 app.get("/heroes", function(req, res) {
     let archivoHeroes = path.resolve("heroes.json")    
     res.sendFile(archivoHeroes)
 })
 
+/* Pagina Creditos */
 app.get("/creditos", function(req, res) {
     res.send(`Developer: Oscar Ariel Saucedo ----
     Trabajo finalizado`)
 })
 
+/* Pagina Default */
 app.get("*", function(req, res) {
     res.send(`Error: Not Found`)
 })
 
+/* Llamado a servidor - Puerto 3000 */
 app.listen(3000)
